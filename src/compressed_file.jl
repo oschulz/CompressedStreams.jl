73b703203a00601505171e7d8479f98cc6377839
Oliver Schulz <oschulz@mpp.mpg.de>
2018-11-16 13:04:00 +0100

    Port to Julia v0.7/v1.0

--- src/compressed_file.jl
+++ src/compressed_file.jl
@@ -10,18 +10,18 @@ Base.open(file::CompressedFile, mode::AbstractString = "r") = begin
 
     if mode == "r"
         if endswith(filename, ".gz")
-            open(`gzip -d -c $filename`, "r", STDOUT)[1]
+            open(`gzip -d -c $filename`, "r")
         elseif endswith(filename, ".bz2")
             try
-                open(`pbzip2 -d -c $filename`, "r", STDOUT)[1]
+                open(`pbzip2 -d -c $filename`, "r")
             catch
                 info("pbzip2 doesn't seem to be available, falling back to standard bzip2")
-                open(`bzip2 -d -c $filename`, "r", STDOUT)[1]
+                open(`bzip2 -d -c $filename`, "r")
             end
         elseif endswith(filename, ".xz")
-            open(`xz -d -c $filename`, "r", STDOUT)[1]
+            open(`xz -d -c $filename`, "r")
         else
-            open(`cat $filename`, "r", STDOUT)[1]
+            open(`cat $filename`, "r")
         end
     else
         @assert false
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 # This file is a part of CompressedStreams.jl, licensed under the MIT License (MIT).
 
-import Compat.Test
+import Test
+
 Test.@testset "Package CompressedStreams" begin
 end
